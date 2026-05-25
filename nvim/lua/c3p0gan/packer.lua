ffdd4bbe0add94b41e7cc22b968c8658d8ed99d6
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2026-05-25 10:54:06 -0300

    fix/updated versions of lsp

--- nvim/lua/c3p0gan/packer.lua
+++ nvim/lua/c3p0gan/packer.lua
@@ -1,50 +1,57 @@
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use 'nvim-lua/plenary.nvim'
 
-  use {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  } 
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use({
-    'folke/tokyonight.nvim', as = 'tokyonight',
-    config = function()
-      vim.cmd('colorscheme tokyonight')
-    end
-  })
+    use {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use {
-    'kdheepak/lazygit.nvim',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+    use({
+        'folke/tokyonight.nvim',
+        as = 'tokyonight',
+        config = function()
+            vim.cmd('colorscheme tokyonight')
+        end
+    })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('tpope/vim-surround')
-  use('tpope/vim-repeat')
-  use {'akinsho/toggleterm.nvim', tag = '*', config = function()
-      require('toggleterm').setup()
-  end}
+    use {
+        'kdheepak/lazygit.nvim',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  -- LSP, Mason, completion (no lsp-zero)
-  use {
-    'neovim/nvim-lspconfig',
-    tag = 'v0.1.8'   -- pinned to avoid the "framework deprecated" warning
-  }
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('tpope/vim-surround')
+    use('tpope/vim-repeat')
+    use { 'akinsho/toggleterm.nvim', tag = '*', config = function()
+        require('toggleterm').setup()
+    end }
 
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-nvim-lsp'
+    -- LSP, Mason, completion
+    use {
+        'neovim/nvim-lspconfig',
+        tag = 'v2.9.0'
+    }
+
+    use {
+        'williamboman/mason-lspconfig.nvim',
+        tag = 'v2.2.0'
+    }
+
+    use 'williamboman/mason.nvim'
+
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-nvim-lsp'
 end)
