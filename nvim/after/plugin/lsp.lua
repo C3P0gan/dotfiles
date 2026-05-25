35e53581649ef76c4300a34a47af85fc129f02db
cristoffer_pogan <cristoffer_pogan@econeteditora.com.br>
2026-05-25 10:53:25 -0300

    fix/updated lsp.lua config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,28 +1,7 @@
--- Mason setup
+-- 1.  Setup Mason
 require('mason').setup()
 
--- mason-lspconfig: automatically install servers and set them up with native config
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'ts_ls',
-        'eslint',
-        'lua_ls',
-        'rust_analyzer',
-        'gopls',
-        'pylsp',
-        'jdtls'
-    },
-    handlers = {
-        -- The default handler: create a minimal vim.lsp.config entry and enable the server
-        function(server_name)
-            -- You can add server-specific settings here later if needed
-            vim.lsp.config(server_name, {})
-            vim.lsp.enable(server_name)
-        end,
-    },
-})
-
--- nvim-cmp setup (auto-completion)
+-- 2. Setup nvim-cmp (Completion)
 local cmp = require('cmp')
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
@@ -38,14 +17,40 @@ cmp.setup({
     }),
 })
 
--- Global LSP keymaps and formatting on attach
+-- 3. Prepare LSP capabilities for nvim-cmp
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- 4. Apply capabilities for nvim-cmp
+vim.lsp.config('*', {
+    capabilities = capabilities,
+})
+
+-- 5. Setup mason-lspconfig for automatic server enabling
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'ts_ls',
+        'eslint',
+        'lua_ls',
+        'rust_analyzer',
+        'gopls',
+        'pylsp',
+        'jdtls',
+    },
+    -- handlers = {
+    --     function(server_name)
+    --         vim.lsp.config(server_name, {})
+    --         vim.lsp.enable(server_name)
+    --     end,
+    -- },
+})
+
+-- 6. Global LSP keymaps and settings using LspAttach
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         local bufnr = args.buf
 
-        -- Keymaps
-        local opts = { buffer = buffnr, remap = false }
+        local opts = { buffer = bfnr, remap = false }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
@@ -59,29 +64,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Formatting keymap (if server supports it)
         if client:supports_method('textDocument/implementation') then
-            vim.keymap.set('n', '<space>f', function()
+            vim.keymap.set('n', '<leader>f', function()
                 vim.lsp.buf.format({ async = true })
             end, opts)
         end
 
-        -- Enable LSP completion (NeoVim 0.11 built-in completion can be enabled with this)
+        -- Enable built-in auto-completion (optional, can co-exist with nvim-cmp
         if client:supports_method('textDocument/completion') then
             vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = false })
         end
     end,
 })
-
--- (Optional) Format on save – uncomment if you want it
--- vim.api.nvim_create_autocmd('LspAttach', {
---   callback = function(args)
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
---     if client:supports_method('textDocument/formatting') then
---       vim.api.nvim_create_autocmd('BufWritePre', {
---         buffer = args.buf,
---         callback = function()
---           vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
---         end,
---       })
---     end
---   end,
--- })
