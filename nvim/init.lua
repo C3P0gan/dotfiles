b3b86fb6c20d0cc2c62c78a3d4b16c6c013d569f
Cristoffer Mário Pogan <cmp.night@gmail.com>
2026-07-24 10:41:54 -0300

    feat/migrated NeoVim configuration from Packer to Lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,9 @@
 require("c3p0gan")
+require("config.lazy")
+
+-- Ensure symbols and ligatures render correctly
+vim.opt.conceallevel = 2
+
+-- Recommended for rich character rendering
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
